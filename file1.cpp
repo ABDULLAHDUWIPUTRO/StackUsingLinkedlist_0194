e47684881c3272e989a2c747f66df7ab3e9c6781
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-05-22 10:32:27 +0700

    //pop the top element from the stack

--- file1.cpp
+++ file1.cpp
@@ -92,3 +92,21 @@ int main()
     cout << "4. Exit\n";
     cout << "Enter your choice: ";
     cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+    cout << "Enter the value to push";
+    cin >> value;
+    stack.push(value); // push the entered value onto the stack
+    break;
+    case 2:
+    if (!stack.isEmpty())
+    {
+        stack.pop(); //pop the top element from the stack
+    }
+    else
+    {
+        cout << "stack is empty. Cannot pop." << endl;
+    }
+    break;
